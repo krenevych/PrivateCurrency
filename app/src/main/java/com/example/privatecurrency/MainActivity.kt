7ad6bbb9cc743490a55439148b427a3f931ef2cc
Andrii Krenevych <krenevych@knu.ua>
2024-11-05 10:04:04 +0200

    ViewModel for Privat app

--- app/src/main/java/com/example/privatecurrency/MainActivity.kt
+++ app/src/main/java/com/example/privatecurrency/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.privatecurrency
 
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.privatecurrency.databinding.ActivityMainBinding
 import com.example.privatecurrency.retrofit.RetrofitObject
@@ -13,32 +14,20 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(binding.root)
 
         binding.button.setOnClickListener {
-            retrofitObject?.getCurrencyExchange {
-                Log.d(TAG, "Currency: $it")
-
-                it?.forEach{currency ->
-                    Log.d(TAG, "$currency")
-                }
-            }
-
+            viewModel.fetchData()
         }
-    }
 
-    private var retrofitObject: RetrofitPrivate? = null
-    override fun onStart() {
-        super.onStart()
-        retrofitObject = RetrofitPrivate()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        retrofitObject = null
+        viewModel.currencyList.observe(this) {
+            Log.d(TAG, "onCreate: $it")
+        }
     }
 
     val TAG = "XXXXX"
